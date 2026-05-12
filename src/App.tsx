bf9aa18419cb35ae908af0eda34191c5cd7e7682
Danil Malikov CEO SWAIP <wynerzhinesfbzn@hotmail.com>
2026-05-12 14:34:35 +0300

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AccessibilityAssistant from './AccessibilityAssistant';
+
+export default function App() {
+  return (
+    <AccessibilityAssistant
+      onBack={() => {}}
+      accent="#a78bfa"
+      apiBase=""
+    />
+  );
+}
